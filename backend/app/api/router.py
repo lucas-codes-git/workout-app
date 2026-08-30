5d5e976ffea49bed5ce1fd78bb296b8af829fe2e
Lucas P <lucaspontes171717@gmail.com>
2026-08-30 11:16:49 -0400

    removed debug comments

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -11,8 +11,3 @@ router.include_router(workouts_router)
 router.include_router(exercises_router)
 router.include_router(workout_exercises_router)
 router.include_router(sets_router)
-
-# example workout_id = 73061739-76df-4471-99ae-76a10d7a1964
-# example exercise_id = 996c352c-794e-4e74-9da1-289d763a2785
-# example workout_exercise_id = 8b3e3abc-24f3-4330-9910-a2eecabc82aa
-# example set_id = dc3954d0-b1e5-46b1-ac84-0a408cc3d757
